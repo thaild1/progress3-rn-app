38406b7dd43631d0d572d392d94e1185b150523c
ldthai <ldthai1@tma.com.vn>
2025-06-29 00:32:35 +0700

    add navigation bottom tab

---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Bookmarks() {
+    return (
+        <View>
+            <Text>Bookmarks screen</Text>
+        </View>
+    )
+}

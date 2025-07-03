5ee0a4d4f36c9bd7a869dd0ba736b1be223cbea6
ldthai1 <ldthai1@tma.com.vn>
2025-07-03 16:19:56 +0700

    add bookmarks

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,9 +1,62 @@
-import { Text, View } from 'react-native';
+import { Loader } from "@/components/loader";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/feed.styles";
+import { useQuery } from "convex/react";
+import { Image } from "expo-image";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Bookmarks() {
-    return (
-        <View>
-            <Text>Bookmarks screen</Text>
-        </View>
-    )
+  const bookmarkedPosts = useQuery(api.bookmarks.getBookmarkedPosts);
+
+  if (bookmarkedPosts === undefined) return <Loader />;
+  if (bookmarkedPosts.length === 0) return <NoBookmarksFound />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Bookmarks</Text>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={{
+          padding: 8,
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {bookmarkedPosts.map((post) => {
+          if (!post) return null;
+          return (
+            <View key={post._id} style={{ width: "33.33%", padding: 1 }}>
+              <Image
+                source={post.imageUrl}
+                style={{ width: "100%", aspectRatio: 1 }}
+                contentFit="cover"
+                transition={200}
+                cachePolicy="memory-disk"
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+function NoBookmarksFound() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: COLORS.background,
+      }}
+    >
+      <Text style={{ color: COLORS.primary, fontSize: 22 }}>
+        No bookmarked posts yet
+      </Text>
+    </View>
+  );
 }
